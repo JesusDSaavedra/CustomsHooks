bfc7f159a1bc20f82c6b4f70cfd79e32a100c6d5
JesusDavid <iam.jesusd88@gmail.com>
2022-07-09 11:24:11 -0500

    Primer commit

---
+++ useCounter.js
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+export const useCounter = ( initialValue = 10 ) => {
+
+    const [ counter, setCounter ] = useState( initialValue );
+
+    const incremet = ( value = 1 ) => {
+        setCounter( (current) => current + value );
+    }
+    const decremet = ( value = 1 ) => {
+        if ( counter === 0 ) return;
+        setCounter( (current) => current - value );
+    }
+    const reset = () => {
+        setCounter( initialValue );
+    }
+
+
+
+    return {
+
+        counter,
+        incremet,
+        decremet,
+        reset,
+
+    }
+
+}
---
+++ useTodo.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+
+const initialState = []
+
+const init = () => {
+    return JSON.parse( localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+
+    const [ todos, dispatch ] = useReducer( todoReducer, initialState, init )
+
+    useEffect(() => {
+      localStorage.setItem('todos', JSON.stringify( todos ));
+    }, [todos])
+    
+
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: 'Add Todo',
+            payload: todo
+        }
+
+        dispatch(action)
+    }
+
+    const handleDeleteTodo = ( id ) => {
+        dispatch({
+            type: 'Remove Todo',
+            payload: id
+        });
+    };
+
+    const handleToggleTodo = ( id ) => {
+        dispatch({
+            type: 'Toggle Todo',
+            payload: id
+        });
+    };
+
+    const todosCount = todos.length;
+
+    const pendingTodosCount = todos.filter( todo => !todo.done).length;
+
+    return {
+        todos,
+        handleDeleteTodo,
+        handleToggleTodo,
+        handleNewTodo,
+        todosCount,
+        pendingTodosCount,
+    }
+}
